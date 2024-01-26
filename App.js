8239f5313f908e6d1d8ff23d9d5b3cb70b8c1cb2
juliaathar <juathar@gmail.com>
2024-01-26 09:03:12 -0300

    atividade finalizada

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Image
-      style ={styles.img}
-      source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/8/8c/SENAI_S%C3%A3o_Paulo_logo.png'}}
+        style={styles.img}
+        source={require('./src/assets/logo.png')}
       />
+
+      <Text style={styles.txtLogin}>Login</Text>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Email</Text>
+        <TextInput
+          style={styles.input}
+          placeholder='Digite seu email...'
+          placeholderTextColor={'#34898F'}
+        />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Senha</Text>
+        <TextInput
+          style={styles.input}
+          placeholder='Digite sua senha...'
+          placeholderTextColor={'#34898F'}
+        />
+        <Text style={styles.recSenha}>Esqueceu sua senha?</Text>
+      </View>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.textButton}>Entrar</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,10 +45,50 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
+  },
+  txtLogin: {
+    fontSize: 20,
+    fontWeight: '700',
+    marginBottom: 20,
+  },
+  inputContainer: {
+    width: 320,
+    marginBottom: 20,
+  },
+  label: {
+    color: '#49B3BA',
+    fontWeight: '700'
+  },
+  input: {
+    width: '100%',
+    height: 53,
+    borderColor: '#49B3BA',
+    borderWidth: 2,
+    borderRadius: 5,
+    padding: 16,
+    fontSize: 14,
+    marginBottom: 5
   },
   img: {
-    width: 200,
-    height: 200
-  }
+    marginBottom: 25,
+  },
+  button: {
+    backgroundColor: '#496BBA',
+    width: 320,
+    height: 44,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 60
+  },
+  textButton: {
+    color: 'white',
+    fontSize: 14,
+    fontWeight: '700'
+  },
+  recSenha: {
+    color: '#8C8A97',
+    textDecorationLine: 'underline'
+  },
 });
